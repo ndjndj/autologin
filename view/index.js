e93f927c8f1e1ae111070258bfa58e1fc38b5551
ndjndj <teq.ndj@gmail.com>
2021-08-16 21:02:19 +0900

    fix

--- view/index.js
+++ view/index.js
@@ -1,6 +1,6 @@
-
+console.log("index.js")
 //ipcRendererモジュールをインポート
 const { ipcRenderer } = require("electron");
 
 //メインプロセスのipcMain.on("test-send")に変数dataを送る
-ipcRenderer.send("test-send", data); 
+ipcRenderer.send("test-send", data);
