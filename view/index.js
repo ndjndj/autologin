42ec71d22590d0595b29173e55f50fb35fae5985
ndjndj <teq.ndj@gmail.com>
2021-08-19 15:13:25 +0900

    sample

--- view/index.js
+++ view/index.js
@@ -2,5 +2,7 @@ console.log("index.js")
 //ipcRendererモジュールをインポート
 const { ipcRenderer } = require("electron");
 
+var sampleValue = "sample1";
+
 //メインプロセスのipcMain.on("test-send")に変数dataを送る
-ipcRenderer.send("test-send", data);
+ipcRenderer.send("test-send", sampleValue);
