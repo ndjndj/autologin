e3bb56221c2b21a8cfa77d344600a0ddd1c48573
ndjndj <teq.ndj@gmail.com>
2021-09-12 21:00:02 +0900

    fix site list

--- view/top.js
+++ view/top.js
@@ -3,16 +3,11 @@ const { ipcRenderer } = require('electron');
 ipcRenderer.send('json-send');
 ipcRenderer.on('json-return', (event, arg) => {
     const site = document.getElementById('site');
-    let siteUrl;
-    let siteTitle;
     let hrefTag;
     for(let [key, value] of Object.entries(arg)) {
-        steUrl = value['url'];
-        siteTitle = value['title'];
-
         hrefTag = document.createElement('a');
-        hrefTag.innerText = siteTitle;
-        hrefTag.href = siteUrl;
+        hrefTag.innerText = value['title'];
+        hrefTag.href = value['url'];
         hrefTag.id = key;
 
         site.appendChild(hrefTag);
