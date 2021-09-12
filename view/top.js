a8923c9ea52f82ffbd8f0ee95e97e5214ebdd818
ndjndj <teq.ndj@gmail.com>
2021-09-12 20:05:04 +0900

    json loop

--- view/top.js
+++ view/top.js
@@ -2,5 +2,10 @@ const { ipcRenderer } = require('electron');
 
 ipcRenderer.send('json-send');
 ipcRenderer.on('json-return', (event, arg) => {
-    console.log(arg);
+    for(let [key, value] of Object.entries(arg)) {
+        steUrl = value['url'];
+        siteTitle = value['title'];
+
+
+    }
 });
